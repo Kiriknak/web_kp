6e0a58f6c84b62619b07def487fc12dded7fb076
Kiriknak <akutank00@gmail.com>
2022-01-25 21:00:44 +0700

    edit migration

--- resources/views/dashboard/barang.blade.php
+++ resources/views/dashboard/barang.blade.php
@@ -58,13 +58,7 @@
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-yellow-600 focus:border-yellow-600 block w-full p-2.5"
                                 placeholder="Search for products">
 
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                class="h-5 w-5 absolute inset-y-0 left-0 m-auto inline-flex items-center"
-                                viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    clip-rule="evenodd" />
-                            </svg>
+
                         </div>
                     </form>
                     <div class="flex items-center sm:justify-end w-full">
